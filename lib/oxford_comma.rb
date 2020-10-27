b329282b906319a59186b5a2e6e2697083c163e2
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-10-27 16:51:00 -0600

    commit tree 4a4461160613b3ee6608f4f9d447ea64129a3624

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        array[0]
+    elsif array.size == 2
+        array.join(" and ")
+    else
+        temp = array.pop
+        string = array.join(", ")
+        string += ", and #{temp}"
+    end
 end
